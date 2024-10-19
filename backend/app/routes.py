1e29649d0f0b162df3a18555ebd1b0d9b962f01a
Kyle Nguyen <knguyen1193@outlook.com>
2024-10-18 22:17:05 -0700

    add some TODO comments

---
+++ backend/app/routes.py
@@ -0,0 +1,42 @@
+# -------------------------------------------------------------#
+# TODO: User Authentication With Sessions and Cookies:
+# - User Login :  Implement user login functionality where
+# a user can log in by providing credentials (username and
+# password). Use sessions and cookies to track and maintain login states.
+# - User Registration: Allow new users to register by
+# providing a username, password, and email.
+# - Session Management: Use Flask's session management to store user
+# session data securely
+# - Logout: Implement logout functionality that clears the session and
+# removes authentication cookies
+# -------------------------------------------------------------#
+
+
+# -------------------------------------------------------------#
+# TODO: CRUD Operations for Inventory:
+# - Create Inventory Item: Allow the creation of new inventory
+# items with fields like item name, description, quantity, and
+# price. With auto creation of ID Read Inventory Items: Provide
+# APIs to list all inventory items or fetch a single item based on
+# its ID.
+# - Update Inventory Item: Allow the modification of an inventory
+# item's details (name, quantity, price, etc.).by id
+# - Delete Inventory Item: Enable deletion of an inventory item by ID
+# -------------------------------------------------------------#
+
+
+# -------------------------------------------------------------#
+# TODO: USER-Specific Inventory Management:
+# - Each Logged-in user will have their own inventory items, ensuring
+# that users can only access and modify their own data.
+# - Use sessions to ensure that only authenticated users can access
+# inventory-related CRUD Operations
+# -------------------------------------------------------------#
+
+
+# -------------------------------------------------------------#
+# TODO: Session and Cookie Security:
+# - Secure user sessions with encryption (Flask Security key)
+# - Implement proper session expiration handing to automatically
+# log out.
+# -------------------------------------------------------------#
